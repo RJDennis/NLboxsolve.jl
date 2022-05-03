0c61cf7ca9228d4c2d4bba3ac7dce4a6d384b9ee
RJDennis <richard.dennis@glasgow.ac.uk>
2022-05-03 10:17:37 +0100

    Update jacvec.jl

--- src/jacvec.jl
+++ src/jacvec.jl
@@ -1,8 +1,5 @@
 function jacvec(f::Function,x::Array{T,1},v::Array{T,1}) where {T <: AbstractFloat}
 
-    #ep = sqrt(1.0+norm(x)*eps(T))/norm(v)
-    #jvec = (f(x+ep*v) - f(x-eps*v))/(2*ep)
-
     ep = 1e-10
     jvec = imag(f(x+ep*im*v))/ep
 
@@ -10,10 +7,7 @@ function jacvec(f::Function,x::Array{T,1},v::Array{T,1}) where {T <: AbstractFlo
 
 end
 
-function jacvec!(f::Function,x::Array{T,1},v::AbstractArray{T,1},jvec::Array{T,1}) where {T <: AbstractFloat}
-
-    #ep = sqrt(1.0+norm(x)*eps(T))/norm(v)
-    #jvec .= (f(x+ep*v) - f(x-ep*v))/(2*ep)
+function jacvec!(jvec::Array{T,1},f::Function,x::Array{T,1},v::AbstractArray{T,1}) where {T <: AbstractFloat}
 
     ep = 1e-10
     jvec .= imag(f(x+ep*im*v))/ep
